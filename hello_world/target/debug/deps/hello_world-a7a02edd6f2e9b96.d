1dc2a52a921d85e54580b8c59051983196ca84c2
madeline <madgrismad@gmail.com>
2024-07-12 19:12:52 -0700

    chore: Remove unnecessary files and dependencies

--- hello_world/target/debug/deps/hello_world-a7a02edd6f2e9b96.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Madeline\Desktop\Rustaceans\hello_world\target\debug\deps\libhello_world-a7a02edd6f2e9b96.rmeta: src\main.rs
-
-C:\Users\Madeline\Desktop\Rustaceans\hello_world\target\debug\deps\hello_world-a7a02edd6f2e9b96.d: src\main.rs
-
-src\main.rs:
